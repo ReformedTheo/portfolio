7fa49c58e06071f0245e43ef020f0c9586638b04
ReformedTheo <lopes.theo22@gmail.com>
2024-03-20 16:24:00 -0300

    changed test

--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,5 @@ import App from './App';
 // Test 2: The "About Me" section is present
 test('renders the About Me section', () => {
   render(<App />);
-  expect(screen.getByText(/I am a Software Developer/i)).toBeInTheDocument();
+  expect(screen.getByText(/software developer with insatiable curiosity! /i)).toBeInTheDocument();
 });
