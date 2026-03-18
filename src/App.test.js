8afab5c5cc6601ada8074888ef7bc16184cbdac2
ReformedTheo <me@theo.tec.br>
2026-03-18 20:09:58 -0300

    one piecefy

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// Test 2: The "About Me" section is present
-test('renders the About Me section', () => {
+test('renders the About section with bio text', () => {
   render(<App />);
-  expect(screen.getByText(/software developer with insatiable curiosity! /i)).toBeInTheDocument();
+  expect(screen.getByText(/Backend developer with proven experience/i)).toBeInTheDocument();
 });
